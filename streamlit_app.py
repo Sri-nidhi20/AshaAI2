a89f8b872a860dc8493b7d1090fb72dac9c4a019
Sri-nidhi20 <163077897+Sri-nidhi20@users.noreply.github.com>
2025-04-17 09:48:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ if menu == "give feedback":
             new_feedback.to_csv(feedback_file, mode='a', header=False, index=False)
         else:
             new_feedback.to_csv(feedback_file, index=False)
-        st.toast(" Thank you for your feedback!🤗🤩", icon="🎉")
+        st.success("🎉 Thank you for your feedback!🤗🤩")
         st.balloons()
 
 # Admin Dashboard
