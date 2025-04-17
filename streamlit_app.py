9205e17f104949296e1534b5bdf6ce4a82241324
Sri-nidhi20 <163077897+Sri-nidhi20@users.noreply.github.com>
2025-04-17 09:10:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
-import pytz
 import os
 import matplotlib.pyplot as plt
 
@@ -41,10 +40,8 @@ if menu == "give feedback":
     ])
 
     if st.button("Submit Feedback"):
-        ist = pytz.timestamp('Asia/Kolkata')
-        timestamp = datetime.now(ist)
         new_feedback = pd.DataFrame({
-            'timestamp': [timestamp],
+            'timestamp': [date.now()],
             'rating': [rating],
             'comment': [comment],
             'user_email': [email],
