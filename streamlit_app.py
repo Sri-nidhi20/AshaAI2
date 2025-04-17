0827d72ed0b14f89c1e3ab5d2195995934de9deb
Sri-nidhi20 <163077897+Sri-nidhi20@users.noreply.github.com>
2025-04-17 10:47:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ if menu == "Give Feedback 😊😐☹️":
             new_feedback.to_csv(feedback_file, mode='a', header=False, index=False)
         else:
             new_feedback.to_csv(feedback_file, index=False)
-        lottie_success = load_lottieurl("https://lottie.host/embed/73099729-1a0e-4d04-b13c-129c1e9ad3aa/WEt2VVYpfi.json")
+        lottie_success = load_lottieurl("https://assets2.lottiefiles.com/packages/lf20_qp1q7mct.json")
         st.success("🎉 Thank you for your feedback!🤗🤩")
         st_lottie(lottie_success, height=200, key="success")
         
