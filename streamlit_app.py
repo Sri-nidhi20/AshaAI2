a0663c8201d8b9144ac05181c718f3f56bd5e142
Sri-nidhi20 <163077897+Sri-nidhi20@users.noreply.github.com>
2025-04-17 09:14:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ if menu == "give feedback":
 
     if st.button("Submit Feedback"):
         new_feedback = pd.DataFrame({
-            'timestamp': [date.now().strftime("%d-%b-%Y")],
+            'timestamp': [datetime.now(ist).strftime("%d-%b-%Y")],
             'rating': [rating],
             'comment': [comment],
             'user_email': [email],
