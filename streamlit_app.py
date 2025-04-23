6d47d2ec8d456f5411ba96436def95272ca2ce12
Sri-nidhi20 <163077897+Sri-nidhi20@users.noreply.github.com>
2025-04-23 06:18:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -298,6 +298,7 @@ Built by *Nidhi 💛* with love and purpose for the *ASHA AI Hackathon 2025*, As
 elif menu == "QUIZ TIME 🤩🥳":
     st.header("It's the Quiz Time!!")
     st.subheader("🎯 Ready, Set, Code! 💻 Time to show off your skills and conquer this quiz like a coding pro! 💥")
+    today = date.today()
     if st.session_state.last_played != today:
         st.session_state.answered_today = False
     st.markdown(f"🔥 **Your Current Streak:** '{st.session_state.streak}' days")
